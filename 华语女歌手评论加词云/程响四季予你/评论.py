07df06584a4113e0cd063a9e1638a4042a564618
zhangxyu2 <87298929+zhangxyu2@users.noreply.github.com>
2021-07-19 09:59:18 +0800

    Add files via upload

---
+++ 华语女歌手评论加词云/程响四季予你/评论.py
@@ -0,0 +1,26 @@
+import time
+import re
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path=r'C:\pythonProject\venv\Scripts\chromedriver.exe')
+driver.get("https://music.163.com/#/song?id=1807537867")  # 歌曲链接
+driver.implicitly_wait(10)  # 隐试等待
+driver.maximize_window()  # 最大化浏览器
+driver.switch_to.frame(0)
+# 下拉页面
+js = 'document.documentElement.scrollTop = document.documentElement.scrollHeight'
+driver.execute_script(js)
+divs = driver.find_elements_by_css_selector('.itm')
+# 定义爬取的评论数
+for click in range(100):
+    divs = driver.find_elements_by_css_selector('.itm')
+    for div in divs:
+        cnt = div.find_element_by_css_selector('.cnt.f-brk').text
+        cnt = cnt.replace('\n', '')
+        cnt = re.findall('：(.*)', cnt)[0]  # 去掉发布评论的人名字
+        # 定义保存路径
+        with open('四季予你.txt', mode='a', encoding='UTF-8') as f:
+            f.write(cnt + '\n')
+    driver.find_element_by_css_selector('.znxt').click()
+    time.sleep(1)
+driver.quit()
---
+++ 华语女歌手评论加词云/邓紫棋超能力/情感.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import snownlp as SnowNLP
+import re
+import os
+import wordcloud
+import jieba
+import imageio
+file = open('超能力.txt', mode='r', encoding='utf-8')
+txt = file.read()
+txt_list = jieba.lcut(txt)
+string = ' '.join(txt_list)
+shan = {'的', '这', '因为', '很', '和', '都', '那', '首歌', '听', '在', '不论', '是否', '我', '你', '了', '啦', '他', '她', '它', '吧', '是', '不是', '有', '没有', '会', '也', '就', '在', '不', '吗', '啊'}
+wc = wordcloud.WordCloud(
+    width=1000,
+    height=700,
+    background_color='white',
+    font_path='msyh.ttc',
+    stopwords=shan
+)
+wc.generate(string)
+wc.to_file('超能力.png')
